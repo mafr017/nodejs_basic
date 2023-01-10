20cfbb773e49d76f6839886de4b832d1698cfee4
M Aditya Fathur Rahman <madityafr3@gmail.com>
2023-01-10 11:15:34 +0700

    03 timer

---
+++ 03_timer.mjs
@@ -0,0 +1,9 @@
+import timers from "timers/promises";
+var name
+console.info(`Tunggu 5 detik bro ${name}`)
+name = await timers.setTimeout(5000, "ujang")
+console.info(`Tunggu 5 detik bro ${name}\n`)
+
+setInterval(() => {
+    console.info(`Start time at ${new Date()}`);
+}, 1000)
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Belajar Node Js Dasar
-#### List Materi
+
+## List Materi
+
 1. Modules
 2. Global Async
 
-Referensi:  [Programmer Zaman Now](https://www.youtube.com/ProgrammerZamanNow)
+Referensi:  [Programmer Zaman Now](https://www.udemy.com/user/eko-kurniawan/)
